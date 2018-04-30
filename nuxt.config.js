58541a0d301f42f38aecacfbe79274881fb4aa91
Andres Mazzo <amazzoccone@bondacom.com>
2018-04-30 15:09:46 -0300

    Install milligram css

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,9 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  css: [
+    'milligram/dist/milligram.min.css',
+  ],
   /*
   ** Customize the progress bar color
   */
