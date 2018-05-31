02836db7bb651078339dd6b761a43fbb542ffc3e
Andres Mazzo <amazzoccone@bondacom.com>
2018-05-31 01:20:34 -0300

    Add title and works ref in index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,33 @@
 <template>
-  <div>
-    <!--TODO: Add title and work number reference-->
+  <div class="content">
+    <div class="row">
+      <div class="works-desktop">
+        <span class="works-ref">
+          <b>{{ '/0' + page }}</b>
+        </span>
+        <span class="works-ref">
+          {{ $t('home.works') }}
+        </span>
+      </div>
+    </div>
+    <div class="row">
+      <h1 class="title">{{ $t('home.title') }}</h1>
+    </div>
+    <div class="row">
+      <div class="works-mobile float-right">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
+  data() {
+    return {
+      page: 1
+    }
+  },
   fetch ({ store, app }) {
     let images = ['/IMG_0546.JPG', 'IMG_0529.JPG', 'IMG_0529.JPG'];
     let pages = images.length;
@@ -36,3 +57,45 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  /* Mobile First Media Queries *//* Mobil
+
+  /* Base style */
+  .content {
+    margin: 0 auto;
+    width: 60%;
+  }
+
+  .works-ref {
+    font-size: 1.2rem;
+    float: right;
+    line-height: 1.2;
+  }
+
+  .title {
+    font-size: 5rem;
+    text-align: center;
+  }
+  .works-desktop {
+    width: 80%;
+    display: none;
+  }
+
+  /* Larger than mobile screen */
+  @media (min-width: 40.0rem) {
+    .works-desktop {
+      display: block;
+    }
+    .works-mobile {
+      display: none;
+    }
+  }
+
+  /* Larger than tablet screen */
+  @media (min-width: 80.0rem) { }
+
+  /* Larger than desktop screen */
+  @media (min-width: 120.0rem) { }
+
+</style>
