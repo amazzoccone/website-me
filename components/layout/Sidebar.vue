f9f3f5dd97d0ca9fc8ee9b0f951bf125d953afaa
Andres Mazzo <amazzoccone@bondacom.com>
2018-06-07 23:59:14 -0300

    Create Border component

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row pointer" @click="clickHandler">
     <div v-if="positionIsRight" class="full-height column column-border">
-      <div v-for="n in params.pages" :key="n" :class="borderClass(n, 'left')" :style="borderStyle"></div>
+      <border :active="this.params.page" :count="params.pages"/>
     </div>
     <div class="column">
       <div class="row full-height">
@@ -12,13 +12,14 @@
       </div>
     </div>
     <div v-if="positionIsLeft" class="full-height column column-border">
-        <div v-for="n in params.pages" :key="n" :class="borderClass(n, 'right')" :style="borderStyle"></div>
+        <border :active="this.params.page" :count="params.pages"/>
     </div>
   </div>
 </template>
 
 <script>
   import ConfigMixin from './ConfigMixin.vue';
+  import Border from './specific/Border.vue';
 
   export default {
     mixins: [ConfigMixin],
@@ -47,11 +48,6 @@
       positionIsRight() {
         return this.params.position === 'right';
       },
-      borderStyle() {
-        return {
-          height: 100/this.params.pages + 'vh'
-        }
-      },
       titleClass() {
         return {
           'content': true,
@@ -64,16 +60,12 @@
       clickHandler(e) {
           this.$bus.$emit('sidebar:clicked', this.params);
       },
-      borderClass(n, position) {
-        return {
-          ['row border border-'+position]: true,
-          [this.color]: true,
-          active: n === this.params.page
-        }
-      },
       close() {
         this.$emit('sidebar-close-clicked');
       }
+    },
+    components: {
+      Border
     }
   }
 </script>
@@ -124,33 +116,6 @@
   .column-border {
       padding: 0;
   }
-
-  .border {
-    margin: 0;
-    width: 100%;
-  }
-  .border-left {
-    border-left: 2px solid #000;
-  }
-  .border-right {
-    border-right: 2px solid #000;
-  }
-
-  /* Color White */
-  .border.white {
-    border-color: rgba(255, 255, 255, 0.4);
-  }
-  .border.white.active {
-    border-color: rgba(255, 255, 255, 0.8);
-  }
-  /* Color Black */
-  .border.black {
-    border-color: rgba(0, 0, 0, 0.4);
-  }
-  .border.black.active {
-    border-color: rgba(0, 0, 0, 0.8);
-  }
-
 
   .rotate-90 {
     -webkit-transform:rotate(-90deg);
---
+++ components/layout/specific/Border.vue
@@ -0,0 +1,62 @@
+<template>
+  <div v-for="n in count" :key="n" :class="borderClass(n, 'left')" :style="borderStyle"></div>
+</template>
+
+<script>
+  export default {
+    props: {
+      active: {
+        type: Number,
+        default: null,
+      },
+      count: {
+        type: Number,
+        default: 1
+      }
+    },
+    computed: {
+      borderStyle() {
+        return {
+          height: 100/this.params.pages + 'vh'
+        }
+      },
+    },
+    methods: {
+      borderClass(n, position) {
+        return {
+          ['row border border-'+position]: true,
+          [this.color]: true,
+          active: n === this.params.page
+        }
+      },
+    }
+  }
+</script>
+
+<style>
+  .border {
+    margin: 0;
+    width: 100%;
+  }
+  .border-left {
+    border-left: 2px solid #000;
+  }
+  .border-right {
+    border-right: 2px solid #000;
+  }
+
+  /* Color White */
+  .border.white {
+    border-color: rgba(255, 255, 255, 0.4);
+  }
+  .border.white.active {
+    border-color: rgba(255, 255, 255, 0.8);
+  }
+  /* Color Black */
+  .border.black {
+    border-color: rgba(0, 0, 0, 0.4);
+  }
+  .border.black.active {
+    border-color: rgba(0, 0, 0, 0.8);
+  }
+</style>
