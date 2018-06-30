b558db5809aa6c16d9774b9b8d85eca1a6e913e5
Andres Mazzo <amazzoccone@bondacom.com>
2018-06-30 19:29:21 -0300

    Create work card component

---
+++ components/WorkCard.vue
@@ -0,0 +1,104 @@
+<template>
+    <div>
+      <div class="work-image">
+        <img :src="this.image"/>
+        <div class="overlay">
+          <div class="more-icon">+</div>
+        </div>
+      </div>
+      <div class="work-info">
+        <div class="work-title">
+          {{ $t(`works.${name}.title`) }}
+        </div>
+        <div class="work-subtitle">
+          <p class="technique">{{ $t(`works.${name}.technique`) }}</p>
+          <p class="dimension">{{ $t(`works.${name}.dimension`) }}</p>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      name: {
+        type: String,
+        required: true
+      },
+      image: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .work-image {
+    position: relative;
+    width: 100%;
+    height: 170px;
+  }
+
+  .overlay {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(255, 255, 255, 0.5);
+    overflow: hidden;
+    text-align: center;
+    width: 0;
+    height: 100%;
+    line-height: 170px;
+    transition: .5s ease;
+  }
+  .overlay > div {
+    display: inline-block;
+    vertical-align: middle;
+    line-height: normal;
+  }
+  .more-icon {
+    font-weight: lighter;
+    font-size: 2.8rem;
+    color: #333;
+  }
+
+  .work-image:hover .overlay {
+    width: 100%;
+  }
+
+  .work-image img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .work-info {
+    font-size: 0.8rem;
+    color: var(--color);
+    margin-top: 15px;
+  }
+  .work-info > div {
+    display: inline-block;
+  }
+
+  .work-title {
+    float: left;
+    font-weight: bold;
+  }
+  .work-subtitle {
+    float: right;
+    margin-right: 10px;
+  }
+  .work-subtitle > p {
+    margin: 0;
+    line-height: 1.1;
+  }
+  .technique {
+
+  }
+  .dimension {
+    font-size: 0.9rem;
+  }
+</style>
