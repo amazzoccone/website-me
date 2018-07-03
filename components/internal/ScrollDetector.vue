0fc022fc9c8edc9298949a0eb39fbf1e2a970c64
Andres Mazzo <amazzoccone@bondacom.com>
2018-07-03 03:04:41 -0300

    Fix creation and deletion of listener in scroll detector

--- components/internal/ScrollDetector.vue
+++ components/internal/ScrollDetector.vue
@@ -6,10 +6,19 @@
 
 <script>
   export default {
+    data() {
+      return {
+        handleScroll: {}
+      }
+    },
     props: {
       settings: {
         type: Object,
         default: () => { return {}; }
+      },
+      disable: {
+        type: Boolean,
+        default: false
       }
     },
     computed: {
@@ -17,28 +26,45 @@
         return this.settings.tolerance || 1;
       },
       timeout() {
-        return this.settings.tolerance || 40;
+        return this.settings.timeout || 40;
       },
       element() {
         return document.getElementById("scroll-detector");
       }
     },
     mounted() {
-      // IE9, Chrome, Safari, Opera
-    	this.element.addEventListener("mousewheel", this.debouncer(e => {
-          this.handleScroll(e);
-      }));
-    	// Firefox
-    	this.element.addEventListener("DOMMouseScroll", this.debouncer(e => {
-          this.handleScroll(e);
-      }));
+      this.createListener();
     },
     beforeDestroy() {
-      window.removeEventListener('scroll', this.handleScroll);
+      this.deleteListener();
     },
     methods: {
-      handleScroll(e) {
-        //Call your scroll handler here.
+      createListener() {
+        this.handleScroll = this.debouncer(e => {
+          this.checkSpeed(e);
+        });;
+
+        this.element.addEventListener("mousewheel", this.handleScroll);       // IE9, Chrome, Safari, Opera
+        this.element.addEventListener("DOMMouseScroll", this.handleScroll);   // Firefox
+      },
+      deleteListener() {
+        this.element.removeEventListener('mousewheel', this.handleScroll);
+        this.element.removeEventListener('DOMMouseScroll', this.handleScroll);
+        this.handleScroll = null;
+      },
+      //Debouncer functions add a delay between an event and a reaction, so scaling and scrolling don't evoke a function dozens of times.
+      debouncer(func, timeout) {
+        var timeoutID , timeout = timeout || this.timeout;
+
+        return function () {
+          var scope = this , args = arguments;
+          clearTimeout( timeoutID );
+          timeoutID = setTimeout( function () {
+              func.apply( scope , Array.prototype.slice.call( args ) );
+          } , timeout );
+        };
+      },
+      checkSpeed(e) {
         let speed = e.deltaY;
 
         if (speed >= this.tolerance) {
@@ -48,17 +74,18 @@
           this.$emit('scroll:down', speed);
         }
       },
-      //Debouncer functions add a delay between an event and a reaction, so scaling and scrolling don't evoke a function dozens of times.
-      debouncer(func, timeout) {
-        var timeoutID , timeout = this.timeout;
-        return function () {
-          var scope = this , args = arguments;
-          clearTimeout( timeoutID );
-          timeoutID = setTimeout( function () {
-            func.apply( scope , Array.prototype.slice.call( args ) );
-        } , timeout );
-        };
-      }
     },
+    watch: {
+      disable(newVal, oldVal) {
+        if (!newVal) {
+          this.createListener()
+        }
+        else if (newVal) {
+          this.deleteListener();
+        }
+
+        return newVal;
+      }
+    }
   }
 </script>
